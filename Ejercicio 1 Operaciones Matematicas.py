f98e428bcab8cfe44217949e8edb8ba536c0cf1f
RogerSteven1104 <rogersteven2013@gmail.com>
2025-06-04 22:20:36 -0500

    Ejercicios

---
+++ Ejercicio 1 Operaciones Matematicas.py
@@ -0,0 +1,17 @@
+print("Ejercicio 1 = (a + b) ** 3")
+print("Ejercicio 2 = (a + 5)/(b - 1)")
+print ("Ingresa el valor de a")
+a = int(input()) # Varable a
+print ("Ingresa el valor de b")
+b = int(input()) # Variable b
+Ejercicio1 = ((a+2) ** 3)
+print("La respuesta del ejercico 1 es: ", int(Ejercicio1))
+Ejercicio2 = (a + 5)/(b -1)
+print("La respuesta del ejercicio 2 es: ", int(Ejercicio2))
+
+
+
+
+
+
+
---
+++ Ejercicio 2 Area del circulo.py
@@ -0,0 +1,15 @@
+# Inicio del ejercicio
+    
+print("Area de un cirulo")
+
+print("Formula: A = PI *( r )** 2")
+print("Ingresa el valor del radio")
+r = float(input()) # Valor del radio
+
+Area = 3.1415 * (r ** 2) # Calculo del area
+
+print(" El area del circulo es: %.2f " % Area)
+
+# Fin del ejercicio
+
+
